495a560364a8a6e7956c9898a13624aa44a388b5
hinamiqi <hinamiqi@gmail.com>
2014-11-18 16:21:33 +0400

    Update super_function.py

--- super_function.py
+++ super_function.py
@@ -1,15 +1,16 @@
 # -*- coding: UTF-8 -*-
 
 I = 1
-J = 1
+J = 3
 
 def super_function(I, J): #координаты тайла
     coordinates = []
     for i in range(I - 1, I + 2):
         for j in range(J - 1, J + 2):
-            if i != j and abs(i - j) < 2:
-               # print('True ', i, j)
+            print(i , j )
+            if i != j and (i - j) % 2 != 0:
                 coordinates.append((i, j)) #возвращает четыре пары координат
+           
     return(coordinates)            
 
 print(super_function(I, J))
