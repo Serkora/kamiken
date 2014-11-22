84425689c7b5c005eaf05d0f600a77fae08d86ca
Serkora <sergey.korabanov@gmail.com>
2014-11-22 12:59:43 +0000

    cleanup

--- pyglet/LAUNCHER_0_0_3.py
+++ pyglet/LAUNCHER_0_0_3.py
@@ -2,11 +2,11 @@
 # -*- coding: UTF-8 -*-
 
 """
-Из kamiken_0_2_0 импортируются все необходимые константы.
+Из kamiken_0_2_1 импортируются все необходимые константы.
 pyglet.app.run тоже нужно импортировать в ланчере, так как иначе он не знает этой команды.
 """
 
-from kamiken_0_2_0 import Board, BOARD_W, BOARD_H, MSG, TILE_SIZE, SQUARE_SIZE, FONT
+from kamiken_0_2_1 import Board, BOARD_W, BOARD_H, MSG, TILE_SIZE, SQUARE_SIZE, FONT
 
 from CLIENT_0_0_3 import Client
 from SERVER_0_0_3 import GameServer
